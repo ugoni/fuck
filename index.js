5d4a86b405ff274f5b1ba743a8bd66627bdf67d9
ugoni <72482818+ugoni@users.noreply.github.com>
2022-02-24 01:26:10 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-function get_name(name){
-  document.write(name+" ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ");
+//var fs = require('fs');
+//var files_img = fs.readdirSync('fuck_img');
+//var files_gif = fs.readdirSync('fuck_gif');
+
+var files_img = ['1 (1)', '1 (2)', '1 (3)', '1 (4)', '1 (5)', '1 (6)', '1 (7)', '1 (8)', '1 (9)']
+var files_gif = ['fuckyou']
+
+function display_gif() {
+  var gifNum = Math.round(Math.random()*(files_gif.length-1));
+  var ogif = document.createElement("iframe");
+  ogif.setAttribute('src', 'fuck_gif/' + files_gif[gifNum] + '.gif');
+  ogif.setAttribute('height', '290px');
+  ogif.setAttribute('width', '400px');
+  document.body.appendChild(ogif);
 }
 
-function display_image(src) {
-  var br =document.createElement('br');
-  var oImg = document.createElement("img");
-  oImg.setAttribute('src', src);
-  oImg.setAttribute('height', '300px');
-  oImg.setAttribute('width', '200px');
+function display_image(){
+  var br = document.createElement('br');
+  var imgNum = Math.round(Math.random()*(files_img.length-1));
+  var oimg = document.createElement('img');
+  oimg.setAttribute('src', 'fuck_img/' + files_img[imgNum] + '.jpg');
+  oimg.setAttribute('height', '300px');
+  oimg.setAttribute('width', '250px');
   document.body.appendChild(br);
-  document.body.appendChild(oImg);
+  document.body.appendChild(oimg)
 }
-function display_gif(src) {
 
-  var oImg = document.createElement("iframe");
-  oImg.setAttribute('src', src);
-  oImg.setAttribute('height', '290px');
-  oImg.setAttribute('width', '400px');
-
-  document.body.appendChild(oImg);
+function get_name(name) {
+  document.write('<h3>' + name + " ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ" + '</h3>');
 }
